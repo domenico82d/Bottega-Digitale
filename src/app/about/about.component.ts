55129993b2e3252b1ea1d1c05358f4753fbf3e6d
Domenico <d.tucci@software-inside.it>
2026-01-12 14:40:49 +0100

    bag fix

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { VisionComponent } from '../vision/vision.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [VisionComponent],  // ✅ Import qui
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
